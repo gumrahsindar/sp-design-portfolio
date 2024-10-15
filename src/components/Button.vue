79f60a7b2b90761844e6f00ed5b7f3b317bfd228
gumrahsindar <gsindar@gmail.com>
2024-10-15 18:56:22 +0300

    feat: add header section

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a href="#">Free Consultation</a>
+</template>
+
+<style scoped>
+a {
+  border-radius: 28px;
+  background-color: var(--clr-black);
+  padding: 14px 45px;
+  font-size: var(--fs-base);
+  color: var(--clr-light-beige);
+  line-height: 1.75;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: var(--clr-purple);
+  }
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Logo from '../assets/images/logo.svg'
+</script>
+
+<template>
+  <a href="/">
+    <img :src="Logo" alt="Logo" width="64" height="64" />
+  </a>
+</template>
+
+<style scoped>
+@media (width < 40rem) {
+  img {
+    width: 48px;
+    height: 48px;
+  }
+}
+</style>
